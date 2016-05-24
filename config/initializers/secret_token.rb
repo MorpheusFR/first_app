3f9e6c47105efd49021ac260de4c66dcc557a1d4
morpheus <morpheus@morpheus-Mont.12.7>
2016-05-24 16:38:06 +0300

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = '7c40c21dddebce80de6d10e42a697d6cc5cc809e7ea60b2ea1b2ba09a04b2cd11ced740991b94b556b68239dd003ef8277101588c955e7f807e6351d550afab4'
